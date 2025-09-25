892b80b9cf6aa15799b558592eded0bafae64391
Anubhab De <154841713+anubhabde07@users.noreply.github.com>
2025-09-25 22:59:41 +0000

    Datatypes

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,20 @@
+let name = "Anubhab";   // String
+let age = 20;   // Number
+let height = 5.9; // Float (also a Number in JavaScript)
+let isStudent = true;   // Boolean
+
+let nullValue = null; // Null
+let undefinedValue; // Undefined
+
+let hobbies = ["reading", "gaming", "coding"]; // Array
+
+let details = {   // Object
+    name : "Anubhab",
+    roll : 9013,
+    age : 20
+};
+
+console.log([name, age, height, isStudent, nullValue, undefinedValue, hobbies, details]);
+
+console.log(typeof details); // Object
+console.log(typeof hobbies); // Object (Arrays are a type of Object in JavaScript)
