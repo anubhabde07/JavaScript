2b61430cf025f5f7dfebb4bfc6c998aa610528dd
Anubhab De <154841713+anubhabde07@users.noreply.github.com>
2025-09-26 09:58:38 +0000

    Conversion Operations

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,40 @@
+// let score = "33";
+
+// console.log(typeof score); // string
+
+// // Convert string to number
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // 33
+
+// // If the string is not a pure number, it will return NaN
+// let score2 = "33abc";
+// let valueInNumber2 = Number(score2);
+// console.log(valueInNumber2); // NaN
+
+// let score3 = null;
+// console.log(typeof score3);
+
+// let valueInNumber3 = Number(score3);
+// console.log(valueInNumber3);
+// console.log(typeof valueInNumber3);
+
+
+
+// let isLoggedIn = 1;
+// console.log(typeof isLoggedIn); // number
+
+// // Convert number to boolean
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn); // boolean
+// console.log(booleanIsLoggedIn); // true
+
+
+
+
+// ***************************  Operations ***************************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue); // -3
