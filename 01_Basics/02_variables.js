3331db1060574b484bedbbdd6abf6ccecab3a558
Anubhab De <154841713+anubhabde07@users.noreply.github.com>
2025-09-26 08:21:39 +0000

    Variables

---
+++ 01_Basics/02_variables.js
@@ -0,0 +1,18 @@
+const pi = 3.14159; // A constant variable, cannot be reassigned
+let radius = 5; // A variable that can be reassigned
+
+// Calculate the area of a circle
+let area = pi * radius * radius;
+console.log("Area of the circle:", area);
+
+
+let parameter = 2 * pi * radius;
+console.log("Parameter of the circle:", parameter);
+
+let length;
+
+console.log([area, parameter, length]);
+
+var name = "Anubhab";
+var name = "De";
+console.log(name); // 'var' allows redeclaration and reassignment
