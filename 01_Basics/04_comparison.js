5ee8b58f7ffa60e5bd5c0cb97965df70d053becd
Anubhab De <154841713+anubhabde07@users.noreply.github.com>
2025-09-26 09:59:47 +0000

    Comparisons

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,17 @@
+console.log(3 + 6);
+
+console.log(3 > 6);
+console.log(6 >= 5);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log("3" + 6);
+console.log("3" - 6);
+console.log("3" * 6);
+console.log("3" / 6);
+
+// ===  Strictly checks (datatype also)
+console.log(3 === 3); // true
+console.log("3" === 3); // false
