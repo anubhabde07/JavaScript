2ebf33f7b22ce50c6dcb9dfd2703949ead11c979
Anubhab De <154841713+anubhabde07@users.noreply.github.com>
2025-10-19 09:01:48 +0000

    While and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+// While loop
+
+let index = 1;
+while(index <= 10){
+    console.log(`Index value is : ${index}`);
+    // index++;
+    index *= 2;
+}
+
+const myArray = ["Thor", "Ironman", "Hulk", "Spiderman"];
+let arrIndex = 0;
+
+while(arrIndex < myArray.length){
+    console.log(myArray[arrIndex]);
+    arrIndex++;
+}
+
+// do while loop
+
+let count = 1;
+
+do{
+    console.log("G.O.A.T. Player");
+    count++;
+} while(count <= 5);
